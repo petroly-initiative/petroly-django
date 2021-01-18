c64d4229cd1c8ad2d8192eb97856f369973f369d
Nawaf Alfaifi <nowaf142011@hotmail.com>
2021-01-18 17:28:14 +0300

    Revert "working on models and forms"

--- account/models.py
+++ account/models.py
@@ -1,17 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-
-
-class UserProfile(models.Model):
-    '''A general user profile info model that extends `User` fields. '''
-
-    # Connect to the admin User object by on-to-one relation
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    # Additional fields
-    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
-
-    # For a nice representation for an object
-    def __str__(self) -> str:
-        return self.user.username
+# Create your models here.
