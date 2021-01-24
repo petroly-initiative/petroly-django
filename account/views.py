d3e8e7c3eaf5abed905745dca58cfcfb6a33e0d9
ziyad00 <ziyad.alotaibe@gmail.com>
2021-01-24 11:13:39 +0300

    ff

--- account/views.py
+++ account/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import  UserRegistrationForm
+from .models import Profile
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("this is the main page.")
+    
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
