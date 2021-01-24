73541b633e1480dc9898d47f46668452c6f59ec3
ziyad00 <ziyad.alotaibe@gmail.com>
2021-01-24 11:18:24 +0300

    ff

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import Profile
 
 # Register your models here.
 
-admin.site.register(UserProfile)
+admin.site.register(rofile)
--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-<<<<<<< HEAD
 from .models import Profile
 
 class UserRegistrationForm(forms.ModelForm):
@@ -18,37 +17,11 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
-=======
-from .models import UserProfile
-
-
-class UserForm(forms.ModelForm):
-    '''A form for needed fields from admin `User` class'''
-
-    password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ("username", 'first_name', 'last_name', 'email', 'password')
->>>>>>> 0589f45200456000b660104ddbd72274b5c20252
 
 
 class UserProfileForm(forms.ModelForm):
     '''A form for the additional `UserProfile` fields'''
     
     class Meta:
-<<<<<<< HEAD
         model = Profile
         fields = ("profile_pic",)
-=======
-        model = UserProfile
-        fields = ("profile_pic",)
-
-
-class LogInForm(forms.ModelForm):
-    '''A Form only for loging in'''
-    
-    # Fields
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
->>>>>>> 0589f45200456000b660104ddbd72274b5c20252
