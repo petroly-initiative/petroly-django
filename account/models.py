089c809d6d8666db5dfa686eb1f9dba92a7da137
ziyad00 <ziyad.alotaibe@gmail.com>
2021-01-24 11:13:39 +0300

    ff

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    '''A general user profile info model that extends `User` fields. '''
+
+    # Connect to the admin User object by on-to-one relation
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Additional fields
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    # For a nice representation for an object
+    def __str__(self) -> str:
+        return self.user.username
