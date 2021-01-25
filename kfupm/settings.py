c3f40245b7271df245221ea38b92ec517d1706b9
Nawaf Alfaifi <nowaf142011@hotmail.com>
2021-01-25 21:50:37 +0300

    creating evalution app

--- kfupm/settings.py
+++ kfupm/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'evaluation'
 ]
 
 MIDDLEWARE = [
