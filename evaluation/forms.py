627f96fb2715cd7e96e1381eb2fa418d19110ffb
Nawaf Alfaifi <nowaf142011@hotmail.com>
2021-01-29 00:58:04 +0300

    adding seach bar

---
+++ evaluation/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import instructor
+
+
+# class insForm(ModelForm):
+# 	class Meta:
+# 		model = instructor
+# 		fields = '__all__'
--- kfupm/settings.py
+++ kfupm/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    'evaluation'
+    'evaluation' ,
+    'django_filters',
+    'bootstrapform',
 ]
 
 MIDDLEWARE = [
