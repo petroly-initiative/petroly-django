33970cad1df9d588c3bd689e53757fdf72176f5c
Nawaf Alfaifi <nowaf142011@hotmail.com>
2021-02-15 22:03:00 +0300

    for ammar

--- evaluation/models.py
+++ evaluation/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
 class instructor(models.Model):
-    
+
     Name = models.CharField(max_length=250)
     department = models.CharField(max_length=200)        # Additional fields
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
     def __str__(self):
-	    return self.Name
+        return self.Name
 
 
 class evaluation(models.Model):
 
     comments = models.CharField(max_length=250)
-    Edate= models.DateTimeField(auto_now_add=True, )
-
+    Edate = models.DateTimeField(auto_now_add=True, )
+    grading = models.IntegerField()
+    teaching = models.IntegerField()
+    personality = models.IntegerField()
     SID = models.CharField(max_length=250)
     IID = models.ForeignKey(instructor, on_delete=models.CASCADE)
--- evaluation/views.py
+++ evaluation/views.py
@@ -1,42 +1,61 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import instructor
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
+from .models import instructor, evaluation
 from django.core.paginator import Paginator
 from django.contrib import messages
 from .filters import insFilter
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 # Create your views here.
-
-
-
 
 
 class searchInstructor(ListView):
     model = instructor
     template_name = 'evaluation/index.html'
-
+    fields = [
+    'Name', 'department',
+    ]
+    # search bar
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['filter'] = insFilter(self.request.GET, queryset=self.get_queryset())
+        context['filter'] = insFilter(
+            self.request.GET, queryset=self.get_queryset())
+
         return context
 
+    def get_object(self, queryset=None):
+        """
+        Returns the object the view is displaying.
+        By default this requires `self.queryset` and a `pk` or `slug` argument
+        in the URLconf, but subclasses can override this to return any object.
+        """
+        # Use a custom queryset if provided; this is required for subclasses
+        # like DateDetailView
+        if queryset is None:
+            queryset = self.get_queryset()
+        # Next, try looking up by primary key.
+        pk = self.request.user
 
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
 
-def search(request):
-    
-    instructor=instructor.objects.all()
-    
-       # Pagintion
-    myFilter = insFilter(request.GET['q'], queryset=instructor)
-    instructor = myFilter.qs 
-    context = {'instructors':instructor,	'myFilter':myFilter}
-    return render(request,'evaluation/instructors.html',context)
+        try:
+            # Get the single item from the filtered queryset
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(("No %(verbose_name)s found matching the query") %
+                        {'verbose_name': queryset.model._meta.verbose_name})
+        return obj
 
-# Create your views here.
-
-def instructors(request):
-    context = {
-       
-    }
-  
-    return render(request, 'evaluation/instructors.html',context)
+    def post(self, request):
+        obj = self.get_object()
+        rating_1 = int(request.POST['rating'])
+        rating_2 = int(request.POST['ratingtwo'])
+        rating_3 = int(request.POST['ratingthree'])
+        comment = request.POST['comment']
+        
+        rating =evaluation(comments=comment,grading=rating_1, teaching=rating_2, personality=rating_3, SID=request.user, IID= obj.pk )
+        
+        
+        total = rating_1 + rating_2 + rating_3
+        print(ID)
+        return HttpResponse()
