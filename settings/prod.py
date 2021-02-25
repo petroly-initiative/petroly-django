5f750f8b18064d61fc998232c07541ee7207d82b
ammar-faifi <a@ammarf.com>
2021-02-26 00:20:50 +0300

    fix prod environ

--- settings/prod.py
+++ settings/prod.py
@@ -4,13 +4,15 @@ from pathlib import Path
 import dj_database_url 
 import django_heroku
 import environ
+import os
 
 env = environ.Env()
 env.read_env()
 
-SECRET_KEY = env.str("SECRET_KEY", default='')
+SECRET_KEY = os.environ.get("SECRET_KEY", default='')
 
 DEBUG = env.bool("DEBUG", default=True)
+DEBUG = os.environ.get("DEBUG", default=True)
 
 ALLOWED_HOSTS = ['ccwebsite1.herokuapp.com']
 
