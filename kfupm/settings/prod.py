4fb41272fec29e5ab5ab7b520f30716f57b7d624
ammar-faifi <a@ammarf.com>
2021-02-26 21:53:50 +0300

    configure prod.py for Cloudinary

--- kfupm/settings/prod.py
+++ kfupm/settings/prod.py
@@ -5,6 +5,7 @@ import dj_database_url
 import django_heroku
 import environ
 import os
+import cloudinary
 
 env = environ.Env()
 env.read_env()
@@ -16,6 +17,13 @@ DEBUG = os.environ.get("DEBUG", default=True)
 
 ALLOWED_HOSTS = ['ccwebsite1.herokuapp.com']
 
+# Clouddinary: for media
+
+cloudinary.config(
+    cloud_name = env.str("CLOUDINARY_NAME", ''), 
+    api_key = env.str("CLOUDINARY_API_KEY", ''), 
+    api_secret = env.str("CLOUDINARY_API_SECRET", '') 
+)
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
