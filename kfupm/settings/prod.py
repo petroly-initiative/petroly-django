1324a6143f8906f194666e91fe31eabb9f079228
ammar-faifi <a@ammarf.com>
2021-03-24 15:10:20 +0300

    update prod.py

--- kfupm/settings/prod.py
+++ kfupm/settings/prod.py
@@ -17,6 +17,12 @@ DEBUG = os.environ.get("DEBUG", default=True)
 
 ALLOWED_HOSTS = ['petroly-main.herokuapp.com']
 
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+
 # Clouddinary: for media
 
 config(
