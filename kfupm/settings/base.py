29b38946d826bdddc883fb0c85560412d9e4b505
ammar-faifi <a@ammarf.com>
2021-03-26 16:56:50 +0300

    reduce passord validtor

--- kfupm/settings/base.py
+++ kfupm/settings/base.py
@@ -63,13 +63,13 @@ WSGI_APPLICATION = 'kfupm.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        # 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        # 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
