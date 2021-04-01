3ac6980c2c3bf678d13d583c7924398afe91c94c
ammar-faifi <a@ammarf.com>
2021-04-01 13:56:26 +0300

    add roommate t0 installed apps

--- kfupm/settings/base.py
+++ kfupm/settings/base.py
@@ -10,6 +10,7 @@ INSTALLED_APPS = [
     'maintenance_mode',
     'account.apps.AccountConfig',
     'evaluation.apps.EvaluationConfig' ,
+    'roommate.apps.RoommateConfig' ,
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
