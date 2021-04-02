3d1aa2ea80b46aa66ce0aac1378cde581a044db7
ammar-faifi <a@ammarf.com>
2021-04-02 08:58:52 +0300

    basic views

--- roommate/views.py
+++ roommate/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Bid
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DeleteView,
+    DetailView,
+    CreateView,
+    UpdateView,
+)
 
-# Create your views here.
+
+class BidCreateView(CreateView):
+    model = Bid
+
+
+class BidDeleteView(DeleteView):
+    model = Bid
+
+
+class BidUpdateView(UpdateView):
+    model = Bid
+
+
+class BidDetailView(DetailView):
+    model = Bid
+
+
+class BidListView(ListView):
+    model = Bid
