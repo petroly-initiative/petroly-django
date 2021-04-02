6859861352d07857ef4d076c7eac5eb6414d980d
ammar-faifi <a@ammarf.com>
2021-04-02 08:59:04 +0300

    first model

--- roommate/models.py
+++ roommate/models.py
@@ -3,8 +3,7 @@ from django.urls import reverse
 from django.utils.translation import gettext as _
 
 
-class RoommateBid(models.Model):
-    
+class Bid(models.Model):
 
     # Basic
     name = models.CharField(_("Name"), max_length=100)
@@ -17,14 +16,14 @@ class RoommateBid(models.Model):
     temperature = models.IntegerField(_("Room Temperature You Like"))
     region = models.CharField(_("Your Hometown"), max_length=50)
 
-
     class Meta:
-        verbose_name = _("roommate_bid")
-        verbose_name_plural = _("roommate_bids")
+        verbose_name = _("bid")
+        verbose_name_plural = _("bids")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("roommate_bid_detail", kwargs={"pk": self.pk})
+        return reverse("bid_detail", kwargs={"pk": self.pk})
+
 
