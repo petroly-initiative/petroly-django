f338b338b46570307a67a6ed44a7d06dd0e2496d
ammar-faifi <a@ammarf.com>
2021-05-31 09:54:50 +0300

    prepare data update populate

--- populate.py
+++ populate.py
@@ -4,12 +4,12 @@ from os import error, name, read
 import kfupm.settings.dev
 from django.conf import settings
 import django
-from cloudinary.uploader import upload
+from cloudinary.uploader import upload_image
 import cloudinary
 
 
 # settings.configure()
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kfupm.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kfupm.settings.dev')
 
 print(os.environ.get('DJANGO_SETTINGS_MODULE'))
 django.setup()
@@ -23,29 +23,42 @@ def populate(name, dep, url):
     new_obj = Instructor.objects.get_or_create(name=name, department=dep)
     
     if url != 'https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png' and url != '':
-        profile_pic = upload(
+        profile_pic = upload_image(
                             url,
                             folder='instructors/profile_pics',
                             public_id=name,
                             overwrite=True,
                             invalidate=True,
                             transformation=[
-                                {'width': 200, 'height': 200, 'gravity': "face", 'crop': "thumb"}
+                                {'width': 200, 'crop': "thumb"}
                             ],
                             format='jpg'
                         )    
-        new_obj[0].profile_pic = profile_pic['url']
+        new_obj[0].profile_pic = profile_pic
     new_obj[0].save()
 
 
     print(new_obj)
-with open('instructor.csv', 'r') as f:
-    reader = csv.DictReader(f, delimiter=',')
 
-    for row in reader:
-        try:
-            populate(row['name'], row['dep'], row['url'])
-        except cloudinary.exceptions.Error as e:
-            populate(row['name'], row['dep'], '')
-            print(e)
-        continue
+deps = [
+    'AE', 'ARC', 'ARE', 'CE', 'CEM', 'CHE', 
+    'CHEM', 'COE', 'CRP', 'EE', 'GS', 'ICS', 'ISOM',
+    'LS', 'MATH', 'ME', 'MGT', 'PE', 'PHYS', 'SE'
+]
+
+for dep in deps:
+    print('\n>>>>>>>>>>>>>>>>>>>>>>>> ' + dep)
+
+    with open('data/'+dep+'.csv', 'r') as file:
+        reader = csv.DictReader(file, delimiter=';')
+
+        for row in reader:
+            try:
+                populate(row['name'], dep, row['url'])
+            except cloudinary.exceptions.Error as e:
+                populate(row['name'], dep, None)
+                print(e)
+            except Exception as e:
+                print(e)
+                exit(-1)
+            continue
