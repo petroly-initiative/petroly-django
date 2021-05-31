3608fad1078640db434b33b10092a0ffd2e15c3a
ammar-faifi <a@ammarf.com>
2021-05-31 17:16:27 +0300

    clean code style

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -8,16 +8,8 @@ app_name = "evaluation"
 urlpatterns = [
     # path('', views.index, name='index'),
     path("instructors/", views.InstructorListView.as_view(), name="instructors"),
-    path(
-        "instructor/create/",
-        views.InstructorCreateView.as_view(),
-        name="instructor_form",
-    ),
-    path(
-        "instructor/<int:pk>/delete",
-        views.InstructorDeleteView.as_view(),
-        name="instructor_delete",
-    ),
+    path("instructor/create/", views.InstructorCreateView.as_view(), name="instructor_form",),
+    path("instructor/<int:pk>/delete", views.InstructorDeleteView.as_view(), name="instructor_delete",),
     path("evaluate/<int:pk>/", views.Evaluate.as_view(), name="evaluate"),
     path("instructor/<int:pk>/", views.InstructorDetailView.as_view(), name="detail"),
     path("my_evaluations/<pk>", views.EvaluationListView.as_view(), name="evaluation_list"),
