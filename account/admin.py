f8cb1bfd9b390ab59487458b8430ee6ce15cd6f0
ammar-faifi <a@ammarf.com>
2021-06-05 09:24:51 +0300

    improve admin site

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Profile
 from django.apps import apps
+from graphql_auth.models import UserStatus
 # Register your models here.
 
-admin.site.register(Profile)
 
-app = apps.get_app_config('graphql_auth')
-for model_name, model in app.models.items():
-    admin.site.register(model)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ['user', 'major', 'year', 'profile_pic']
+
+
+# app = apps.get_app_config('graphql_auth')
+# for model_name, model in app.models.items():
+#     admin.site.register(model)
+@admin.register(UserStatus)
+class UserStatusAdmin(admin.ModelAdmin):
+
+    list_display = ['user', 'verified', 'archived']
+    list_filter = ['verified']
