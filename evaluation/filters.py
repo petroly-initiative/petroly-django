a17b19f12a3ecd9b11c4c3be6cdc3fa14b044c3a
ziyad00 <ziyad.alotaibe@gmail.com>
2021-06-08 20:16:49 +0300

    gg

--- evaluation/filters.py
+++ evaluation/filters.py
@@ -5,18 +5,18 @@ from django import forms
 from data import departments
 
 
-#class InstructorFilter(django_filters.FilterSet):
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.filters['Name'].field.widget.attrs.update({'class': '', 'placeholder':'Email'})
-    #     self.filters['Name'].
+class InstructorFilter(django_filters.FilterSet):
+     def __init__(self, *args, **kwargs):
+         super().__init__(*args, **kwargs)
+         self.filters['Name'].field.widget.attrs.update({'class': '', 'placeholder':'Email'})
+         self.filters['Name'].
 
-    # Name = django_filters.CharFilter(label='Name', widget=forms.TextInput(attrs={'class': 'searchbar',
-    #                                                                                 'placeholder':'Search Here !',
-    # 'style':"padding: 16px;margin: 0px;color: var(--dark);margin-left: 2px;text-align: center;font-size: 1.0rem;border-radius: 0px;height: 41px;"}))
+     Name = django_filters.CharFilter(label='Name', widget=forms.TextInput(attrs={'class': 'searchbar',
+                                                                                     'placeholder':'Search Here !',
+     'style':"padding: 16px;margin: 0px;color: var(--dark);margin-left: 2px;text-align: center;font-size: 1.0rem;border-radius: 0px;height: 41px;"}))
 
-    #department = django_filters.ChoiceFilter(label="department", choices=departments)
+    department = django_filters.ChoiceFilter(label="department", choices=departments)
 
-   # class Meta:
-    #    model = Instructor
-     #   fields = {"name": ["icontains"]}
+    class Meta:
+        model = Instructor
+        fields = {"name": ["icontains"]}
