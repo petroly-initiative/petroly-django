828c2c62fb686906bb9446e0c7dbcfbcceddf682
ammar-faifi <a@ammarf.com>
2021-06-08 20:19:50 +0300

    return the needed code back

--- evaluation/filters.py
+++ evaluation/filters.py
@@ -6,14 +6,14 @@ from data import departments
 
 
 class InstructorFilter(django_filters.FilterSet):
-     def __init__(self, *args, **kwargs):
-         super().__init__(*args, **kwargs)
-         self.filters['Name'].field.widget.attrs.update({'class': '', 'placeholder':'Email'})
-         self.filters['Name'].
+    #  def __init__(self, *args, **kwargs):
+    #      super().__init__(*args, **kwargs)
+    #      self.filters['Name'].field.widget.attrs.update({'class': '', 'placeholder':'Email'})
+    #      self.filters['Name'].
 
-     Name = django_filters.CharFilter(label='Name', widget=forms.TextInput(attrs={'class': 'searchbar',
-                                                                                     'placeholder':'Search Here !',
-     'style':"padding: 16px;margin: 0px;color: var(--dark);margin-left: 2px;text-align: center;font-size: 1.0rem;border-radius: 0px;height: 41px;"}))
+    #  Name = django_filters.CharFilter(label='Name', widget=forms.TextInput(attrs={'class': 'searchbar',
+    #                                                                                  'placeholder':'Search Here !',
+    #  'style':"padding: 16px;margin: 0px;color: var(--dark);margin-left: 2px;text-align: center;font-size: 1.0rem;border-radius: 0px;height: 41px;"}))
 
     department = django_filters.ChoiceFilter(label="department", choices=departments)
 
