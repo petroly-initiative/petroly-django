a1428def277604b68e3368055f745b7a5f675e3b
ziyad00 <ziyad.alotaibe@gmail.com>
2021-06-11 21:41:41 +0300

    add mutuation

--- evaluation/forms.py
+++ evaluation/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from .models import Instructor
 
 
-# class insForm(ModelForm):
-# 	class Meta:
-# 		model = instructor
-# 		fields = '__all__'
+class insForm(ModelForm):
+ 	class Meta:
+ 		model = instructor
+ 		fields = '__all__'
