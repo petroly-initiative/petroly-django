840f763bed94f21ef6b28847a753852a59c07013
ammar-faifi <a@ammarf.com>
2021-06-18 22:05:09 +0300

    add roommate.schema

--- kfupm/schema.py
+++ kfupm/schema.py
@@ -3,13 +3,16 @@ import graphene
 import evaluation.schema
 import account.schema
 import forum.schema
+import roommate.schema
 
-class Query(evaluation.schema.Query, account.schema.Query, forum.schema.Query, graphene.ObjectType):
+class Query(evaluation.schema.Query, account.schema.Query, forum.schema.Query, 
+            roommate.schema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
 
-class Mutation(evaluation.schema.Mutation, account.schema.Mutation, forum.schema.Mutation, graphene.ObjectType):
+class Mutation(evaluation.schema.Mutation, account.schema.Mutation, forum.schema.Mutation, 
+                roommate.schema.Mutation, graphene.ObjectType):
 
     pass
 
