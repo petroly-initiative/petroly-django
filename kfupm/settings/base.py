9272f2a2d2f6f884b0638c18543a98a68aac4d7e
ammar-faifi <a@ammarf.com>
2021-06-19 10:45:04 +0300

    add middleware and ginored url errors

--- kfupm/settings/base.py
+++ kfupm/settings/base.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
