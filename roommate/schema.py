d381ed1f9625d22274cdb2bfedc410a361f92bf7
ammar-faifi <a@ammarf.com>
2021-06-19 10:23:51 +0300

    add CRUD methods, not tested

--- roommate/schema.py
+++ roommate/schema.py
@@ -1,22 +1,55 @@
 import graphene
+import graphql
+from graphql import GraphQLError
+from graphene import ObjectType
 from graphene_django import DjangoObjectType
-from . import models
+from graphene_django_crud import DjangoGrapheneCRUD
+from graphql_jwt.decorators import *
 
-class OfferType(DjangoObjectType):
+from .models import Offer
+from .utils import is_owner
+
+class OfferType(DjangoGrapheneCRUD):
 
     class Meta:
-        model = models.Offer
-        fields = [
-            'name', 'phone', 'email'
-        ]
+        model = Offer
+        exclude_fields = ('user', )
+        input_exclude_fields = ('user', )
+
+    @classmethod
+    def get_queryset(cls, parent, info, **kwargs):
+        print(info.context.META)
+        print(info.context.POST)
+        print(info.context.GET)
+        return super().get_queryset(parent, info, **kwargs)
+
+    @classmethod
+    @login_required
+    def before_create(cls, parent, info, instance, data):
+        if info.context.user.offer:
+            raise GraphQLError('You already have created an offer')
+
+    @classmethod
+    @is_owner
+    def before_update(cls, parent, info, instance, data):
+        return
+    
+    @classmethod
+    @is_owner
+    def before_delete(cls, parent, info, instance, data):
+        return
+
+class Query(ObjectType):
+
+    offers = OfferType.BatchReadField()
 
 
-class Query(graphene.ObjectType):
 
-    all_offers = graphene.List(OfferType)
+class Mutation(ObjectType):
+    
+    offer_create = OfferType.CreateField()
+    offer_update = OfferType.UpdateField()
+    offer_delete = OfferType.DeleteField()
 
-    def resolve_all_offers(root, info):
-        return models.Offer.objects.all()
 
-
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
