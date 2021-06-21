50d7db6e333394ce6de6a10ebfc78fcfd1697fb0
ammar-faifi <a@ammarf.com>
2021-06-21 16:50:36 +0300

    TextField to CharField, add verbose_name

--- whatsapp/models.py
+++ whatsapp/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+from django.utils.translation import gettext as _
 
 
 class Group(models.Model):    
-    link = models.URLField()
-    major = models.TextField()
-    course = models.TextField()
-    report = models.IntegerField(default=0)
-    verified = models.BooleanField(default=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
+    link = models.URLField(_('Link'))
+    major = models.CharField(_('Major'), max_length=10)
+    course = models.CharField(_('Course'), max_length=10)
+    report = models.IntegerField(_('Report'), default=0)
+    verified = models.BooleanField(_('Verified'), default=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                            verbose_name=_("User"), default=None)
 
     def __str__(self):
         return f'{self.course}'
