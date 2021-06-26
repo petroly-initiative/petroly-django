dab1d51a7533f7591d18419b80fd906d77f6eac2
ammar-faifi <a@ammarf.com>
2021-06-26 05:45:07 +0300

    ignore ads.txt in reporting

--- kfupm/settings/prod.py
+++ kfupm/settings/prod.py
@@ -70,6 +70,7 @@ IGNORABLE_404_URLS = [
     re.compile(r'^/apple-touch-icon.*\.png$'),
     re.compile(r'^/favicon\.ico$'),
     re.compile(r'^/robots\.txt$'),
+    re.compile(r'^/ads\.txt$'),
 ]
 
 django_heroku.settings(locals())
