7b811e0422a864850ac5090ede8399e3efbb25fd
ammar-faifi <a@ammarf.com>
2021-08-26 08:36:00 +0300

    check the match logged user and provided one

--- evaluation/schema.py
+++ evaluation/schema.py
@@ -1,3 +1,4 @@
+from inspect import indentsize
 from typing import Dict, Any
 from django.db.models.base import Model
 import json
@@ -13,9 +14,9 @@ from graphql import GraphQLError
 from graphene_django_crud.types import DjangoGrapheneCRUD, resolver_hints
 from graphene_django_crud.utils import is_required
 
+from graphql_jwt.decorators import login_required
 from django.contrib.auth.models import User, Group
 from graphql_auth.constants import Messages
-from graphql_auth.decorators import login_required
 from . import models
 from .utils import is_owner
 from data import departments
@@ -89,11 +90,16 @@ class EvaluationType(DjangoGrapheneCRUD):
         exclude_fields = ('user', )
 
     @classmethod
+    @login_required
     def before_create(cls, parent, info, instance, data) -> None:
-        pk = data['instructor']['connect']['id']['equals']
+        instructor_pk = data['instructor']['connect']['id']['equals']
+        user_pk = data['user']['connect']['id']['equals']
+
+        if info.context.user.pk != user_pk:
+            raise GraphQLError("The logged user and the provided user must match.")
         
-        if models.Evaluation.objects.filter(user=info.context.user, instructor__pk=pk):
-            raise GraphQLError("You have evaluated this instructor before, you can edit it in My Evaluations")
+        if models.Evaluation.objects.filter(user=info.context.user, instructor__pk=instructor_pk):
+            raise GraphQLError("You have evaluated this instructor before, you can edit it in My Evaluations.")
         return
 
     # Forbid user to change other users' evaluation
