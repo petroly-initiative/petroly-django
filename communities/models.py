720726097ee8d37b239f6ac2628c1db0861dbed5
ammar-faifi <a@ammarf.com>
2022-01-05 09:22:33 +0300

    lazy trans

--- communities/models.py
+++ communities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Community(models.Model):
