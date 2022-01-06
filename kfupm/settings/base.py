88744e91002845c319fc9b6cf3cbcd321968e465
ammar-faifi <a@ammarf.com>
2022-01-06 07:14:23 +0300

    add LocaleMiddleware

--- kfupm/settings/base.py
+++ kfupm/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
