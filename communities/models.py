e7e6114d8dc2fda5c24c899847a5490ed3991f6e
ammar-faifi <a@ammarf.com>
2022-01-10 20:18:19 +0300

    reports to be M2M, arvhieve for 6 reports

--- communities/models.py
+++ communities/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
@@ -22,18 +22,20 @@ class Community(models.Model):
     platform =  models.CharField(_('Platform'), max_length = 12, choices = platforms)
     category = models.CharField(_('Category'), max_length = 12, choices = community_categories)
     section = models.CharField(_('Section'), max_length=10, default="", blank=True) 
-    report = models.IntegerField(_('Report'), default=0)
     verified = models.BooleanField(_('Verified'), default=True)
     archived = models.BooleanField(_('Archived'), default=False)
+    reports = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_("reports"), 
+        related_name='reported_communities', blank=True)
     likes = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_("likes"), 
-        related_name='liked_communities',  default=None, blank=True)
+        related_name='liked_communities', blank=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
-        related_name='owned_communities', verbose_name=_("owner"), null=True, default=None)
+        related_name='owned_communities', verbose_name=_("owner"), null=True)
 
     def __str__(self):
-        return f'{self.name} - {self.report}'
+        return f'{self.name}'
 
-@receiver(post_save, sender=Community)
-def create_profile(sender, instance, created, **kwargs):
-    if instance.report==6:
-        instance.delete()
+@receiver(pre_save, sender=Community)
+def archive_community(sender, instance, **kwargs):
+    num = instance.reports.count()  # count of prevoius reports
+    if num + 1 == 6:
+        instance.archived = True
