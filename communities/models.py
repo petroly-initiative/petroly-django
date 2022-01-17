0abbd795a37a9a34dac0a9e9a5dc1f38c325b69e
Adnan <adnan66b@gmail.com>
2022-01-17 20:51:11 +0300

    Create report model

--- communities/models.py
+++ communities/models.py
@@ -41,8 +41,8 @@ class Community(models.Model):
     verified = models.BooleanField(_('Verified'), default=True)
     archived = models.BooleanField(_('Archived'), default=False)
     icon = CloudinaryFieldCostom(_('icon'), default=None, null=True,  blank=True, max_length=350)
-    reports = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_("reports"), 
-        related_name='reported_communities', blank=True)
+    # reports = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_("reports"), 
+    #     related_name='reported_communities', blank=True)
     likes = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_("likes"), 
         related_name='liked_communities', blank=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
@@ -51,12 +51,28 @@ class Community(models.Model):
     def __str__(self):
         return f'{self.name}, {self.link}'
 
-@receiver(m2m_changed, sender=Community.reports.through)
-def archive_community(sender, instance, action, **kwargs):
-    if action == 'post_add':
-        num = instance.reports.count()  # count of reports
+# This Should be in report model
+# @receiver(m2m_changed, sender=Community.reports.through)
+# def archive_community(sender, instance, action, **kwargs):
+#     if action == 'post_add':
+#         num = instance.reports.count()  # count of reports
 
-        if num >= 2:
-            instance.archived = True
-            instance.save()
+#         if num >= 2:
+#             instance.archived = True
+#             instance.save()
             
+class Report(models.Model):
+    reasons = (
+        ('content', _('Content')),
+        ('link', _('invalidLink')),
+        ('other', _('other')),
+    )
+    reporter = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    reason = models.CharField(_('Reason'), max_length = 8, choices = reasons)
+    other_reason = models.CharField(_('Other_reason'), max_length=10, default="", blank=True) 
+    community = models.ForeignKey(Community, on_delete=models.CASCADE,
+        related_name='reports', verbose_name=_("community"), null=True)
