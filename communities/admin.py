3707fa66722eb163cfbd7e633387d4bb4ca1007e
ammar-faifi <a@ammarf.com>
2022-01-19 15:05:28 +0300

    improve admin

--- communities/admin.py
+++ communities/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Community)
-admin.site.register(models.Report)
+@admin.register(models.Community)
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'verified', 'archived', 'date',
+        'owner']
+    list_filter = ['verified', 'archived']
+    actions = ['make_verified', 'make_unverified', 
+        'make_archived']
+
+    def make_verified(self, request, queryset):
+        queryset.update(verified=True)
+
+    def make_unverified(self, request, queryset):
+        queryset.update(verified=False)
+
+    def make_archived(self, request, queryset):
+        queryset.update(archived=True)
+
+    def make_unarchived(self, request, queryset):
+        queryset.update(archived=False)
+
+@admin.register(models.Report)
+class ReportAdmin(admin.ModelAdmin):
+        list_display = ['community', 'reason', 'reporter', 'created_on']
+        list_filter = ['reason', 'community']
+        # actions = ['make_verified', 'make_unverified', 
+        #     'make_archived']
