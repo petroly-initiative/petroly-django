1101e18395e872a35d43f074ffc1e1021a262c72
ammar-faifi <a@ammarf.com>
2022-01-19 22:36:09 +0300

    create new mutaion for interactions

--- middleware/schema.py
+++ middleware/schema.py
@@ -6,5 +6,5 @@ class HideIntrospectMiddleware:
     """
     def resolve(self, next, root, info, **args):
         if info.field_name == '__schema':
-            return None
+            pass
         return next(root, info, **args)
