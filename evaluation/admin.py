8806378ff212ea1eac7d35012a7f0722694ccd29
ammar-faifi <a@ammarf.com>
2022-01-21 10:27:27 +0300

    add search fielf and date filter

--- evaluation/admin.py
+++ evaluation/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from .models import Instructor, Evaluation
 
+
 @admin.register(Evaluation)
 class EvaluationAdmmin(admin.ModelAdmin):
+    search_fields = ["instructor__name"]
+    list_display = ["user", "instructor", "comment", "date"]
 
-    list_display = ['user', 'instructor', 'comment']
 
 @admin.register(Instructor)
 class InstructorAdmin(admin.ModelAdmin):
-    search_fields = ['name']
-    list_display = ['name', 'department']
-    list_filter = ['department']
+    search_fields = ["name"]
+    list_display = ["name", "department"]
+    list_filter = ["department"]
