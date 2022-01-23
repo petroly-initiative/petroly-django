eec8712d032437560cb93d5347b4e94c1ab42cc9
ammar-faifi <a@ammarf.com>
2022-01-23 18:25:18 +0300

    descending order by likes

--- communities/schema.py
+++ communities/schema.py
@@ -14,16 +14,16 @@ from graphql_jwt.decorators import login_required
 from cloudinary.models import CloudinaryField
 from cloudinary.uploader import upload_image
 from django.contrib.sites.shortcuts import get_current_site
-from django.conf import settings
+from django.db.models import Count
 
 
 @convert_django_field.register(CloudinaryField)
 def convert_CloudinaryField_to_file(field, registry=None, input_flag=None):
-    '''
+    """
     Register the icon filed of type `CloudinaryField`.
-    '''
+    """
     if input_flag:
-        if input_flag == 'create' or input_flag == 'update':
+        if input_flag == "create" or input_flag == "update":
             return FileInput(
                 description=field.help_text or field.verbose_name,
                 required=is_required(field) and input_flag == "create",
@@ -36,23 +36,29 @@ def convert_CloudinaryField_to_file(field, registry=None, input_flag=None):
     )
 
 
-
-
 class CommunityType(DjangoGrapheneCRUD):
     class Meta:
         model = Community
-        input_exclude_fields = ('verified', 'owner')
+        input_exclude_fields = ("verified", "owner")
 
+    @classmethod
+    def get_queryset(cls, parent, info, **kwargs):
+        # descending order for number of likes
+        return (
+            Community.objects.filter(archived=False)
+            .annotate(num_likes=Count("likes"))
+            .order_by("-num_likes")
+        )
 
     @classmethod
     @login_required
     def after_mutate(cls, parent, info, instance: Community, data):
-        if 'icon' in data.keys() and data['icon'].upload:
+        if "icon" in data.keys() and data["icon"].upload:
             try:
                 # to prvent colliding with dev & prod
-                ext =  get_current_site(info.context).domain
+                ext = get_current_site(info.context).domain
                 instance.icon = upload_image(
-                    data['icon'].upload,
+                    data["icon"].upload,
                     folder=f"communities/{ext}/icons",
                     public_id=instance.pk,
                     overwrite=True,
@@ -62,31 +68,35 @@ class CommunityType(DjangoGrapheneCRUD):
                 )
                 instance.save()
             except Exception as e:
-                raise GraphQLError(_('Error while uploading the icon'))
-    
+                raise GraphQLError(_("Error while uploading the icon"))
+
     @classmethod
     def before_create(cls, parent, info, instance, data):
-       instance.owner = info.context.user   # owener is the logged user
-    
+        instance.owner = info.context.user  # owener is the logged user
+
     @classmethod
     def before_update(cls, parent, info, instance, data):
-        if 'icon' in data.keys() and data['icon'].upload is None:
+        if "icon" in data.keys() and data["icon"].upload is None:
             # remove the None value of icon to keep the old one
-            del data['icon']
+            del data["icon"]
 
 
 class ReportType(DjangoGrapheneCRUD):
     class Meta:
         model = Report
-        input_exclude_fields = ('reporter', 'created_on')
-    
+        input_exclude_fields = ("reporter", "created_on")
+
     @classmethod
     @login_required
     def before_create(cls, parent, info, instance, data):
-       instance.reporter = info.context.user   # reporter is the logged user
-       community = Community.objects.get(pk=data['community']['connect']['id']['exact'])
-       if (Report.objects.filter(reporter=instance.reporter, community=community).exists()):
-         raise GraphQLError("You have reported this community Already")
+        instance.reporter = info.context.user  # reporter is the logged user
+        community = Community.objects.get(
+            pk=data["community"]["connect"]["id"]["exact"]
+        )
+        if Report.objects.filter(
+            reporter=instance.reporter, community=community
+        ).exists():
+            raise GraphQLError("You have reported this community Already")
 
 
 class InteractedCommunityMutation(graphene.Mutation):
@@ -101,8 +111,10 @@ class InteractedCommunityMutation(graphene.Mutation):
     def mutate(root, info, ID):
         user = info.context.user
         interactions = {}
-        interactions['liked'] = Community.objects.filter(pk=ID, likes=user).exists()
-        interactions['reported'] = Community.objects.filter(pk=ID, reports__reporter=user).exists()
+        interactions["liked"] = Community.objects.filter(pk=ID, likes=user).exists()
+        interactions["reported"] = Community.objects.filter(
+            pk=ID, reports__reporter=user
+        ).exists()
         return InteractedCommunityMutation(**interactions)
 
 
@@ -116,7 +128,7 @@ class Query(graphene.ObjectType):
 class ToggleLikeCommunity(graphene.Mutation):
     class Arguments:
         ID = graphene.ID()
-    
+
     ok = graphene.Boolean()
 
     @staticmethod
@@ -130,20 +142,21 @@ class ToggleLikeCommunity(graphene.Mutation):
         has_liked = Community.objects.filter(pk=ID, likes__pk=user.pk).exists()
 
         if has_liked:
-           likes.remove(user)   # Unlike
+            likes.remove(user)  # Unlike
         else:
-           likes.add(user)      # Like
+            likes.add(user)  # Like
 
-        return ToggleLikeCommunity(ok = True)
+        return ToggleLikeCommunity(ok=True)
 
 
 class Mutation(graphene.ObjectType):
     community_create = CommunityType.CreateField()
     community_update = CommunityType.UpdateField()
     community_delete = CommunityType.DeleteField()
-    
-    report_create    = ReportType.CreateField()
+
+    report_create = ReportType.CreateField()
 
     has_interacted_community = InteractedCommunityMutation.Field()
     toggle_like_community = ToggleLikeCommunity.Field(
-        description='This will toggle the community like for the logged user')
+        description="This will toggle the community like for the logged user"
+    )
