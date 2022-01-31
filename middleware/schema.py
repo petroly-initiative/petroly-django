1919bc679f043ddc8d4beb34e082822996daf63f
ammar-faifi <a@ammarf.com>
2022-01-31 15:29:31 +0300

    fix Schem middleware

--- middleware/schema.py
+++ middleware/schema.py
@@ -1,10 +1,15 @@
+from graphql import GraphQLError
+from django.conf import settings
+
+
 class HideIntrospectMiddleware:
     """
     This middleware to prevent getting the schema. This class hide the
     introspection. It checks for __schema field_name and __type, if it is there
     return None
     """
+
     def resolve(self, next, root, info, **args):
-        if info.field_name == '__schema':
-            pass
+        if info.field_name in ["__schema", "__type"] and not settings.DEBUG:
+            raise GraphQLError("You are not allowed")
         return next(root, info, **args)
