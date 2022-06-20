faaf9b76f5d19f3dd5ca7a0f8ab504c37e4550f6
ammar-faifi <a@ammarf.com>
2022-06-20 15:02:08 +0300

    comment out old code, prep for replacing

--- evaluation/schema.py
+++ evaluation/schema.py
@@ -1,22 +1,18 @@
-from inspect import indentsize
-from typing import Dict, Any
-from django.db.models.base import Model
-import json
+# import graphene
+# from graphene import *
+# from graphene.types.scalars import String
+# from graphene_django import DjangoObjectType
+# from graphene_django.filter import DjangoFilterConnectionField
+# from graphene_django.forms.mutation import DjangoModelFormMutation
+# from graphene_file_upload.scalars import Upload
+# from graphql import GraphQLError
+# from graphene_django_crud.types import DjangoGrapheneCRUD, resolver_hints
+# from graphene_django_crud.utils import is_required
 
-import graphene
-from graphene import *
-from graphene.types.scalars import String
-from graphene_django import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
-from graphene_django.forms.mutation import DjangoModelFormMutation
-from graphene_file_upload.scalars import Upload
-from graphql import GraphQLError
-from graphene_django_crud.types import DjangoGrapheneCRUD, resolver_hints
-from graphene_django_crud.utils import is_required
+# from graphql_jwt.decorators import login_required
+# from graphql_auth.constants import Messages
 
-from graphql_jwt.decorators import login_required
 from django.contrib.auth.models import User, Group
-from graphql_auth.constants import Messages
 from .models import Evaluation, Instructor
 from .utils import is_owner
 from data import departments
@@ -24,148 +20,148 @@ from data import departments
 
 
 
-class InstructorType(DjangoGrapheneCRUD):
-    '''
-    A type for the `evaluation.Instructor` model. 
-    '''
+# class InstructorType(DjangoGrapheneCRUD):
+#     '''
+#     A type for the `evaluation.Instructor` model. 
+#     '''
 
-    class Meta:
-        model = Instructor
+#     class Meta:
+#         model = Instructor
 
-    grading_avg = graphene.Int()
-    teaching_avg = graphene.Int()
-    personality_avg = graphene.Int()
-    overall_float = graphene.Float()
-    overall = graphene.Int()
+#     grading_avg = graphene.Int()
+#     teaching_avg = graphene.Int()
+#     personality_avg = graphene.Int()
+#     overall_float = graphene.Float()
+#     overall = graphene.Int()
 
-    @staticmethod
-    def resolve_grading_avg(parent, info):
-        return parent.avg()['grading__avg']
+#     @staticmethod
+#     def resolve_grading_avg(parent, info):
+#         return parent.avg()['grading__avg']
     
-    @staticmethod
-    def resolve_teaching_avg(parent, info):
-        return parent.avg()['teaching__avg']
+#     @staticmethod
+#     def resolve_teaching_avg(parent, info):
+#         return parent.avg()['teaching__avg']
     
-    @staticmethod
-    def resolve_personality_avg(parent, info):
-        return parent.avg()['personality__avg']
+#     @staticmethod
+#     def resolve_personality_avg(parent, info):
+#         return parent.avg()['personality__avg']
     
-    @staticmethod
-    def resolve_overall(parent, info):
-        return parent.avg()['overall']
+#     @staticmethod
+#     def resolve_overall(parent, info):
+#         return parent.avg()['overall']
     
-    @staticmethod
-    def resolve_overall_float(parent, info):
-        return parent.avg()['overall_float']
+#     @staticmethod
+#     def resolve_overall_float(parent, info):
+#         return parent.avg()['overall_float']
 
-    @classmethod
-    def before_create(cls, parent, info, instance, data):
-        user: User = info.context.user
-        if user.has_perm("evaluation.add_instructor"):
-            raise GraphQLError("You don't have permission")
-        return
+#     @classmethod
+#     def before_create(cls, parent, info, instance, data):
+#         user: User = info.context.user
+#         if user.has_perm("evaluation.add_instructor"):
+#             raise GraphQLError("You don't have permission")
+#         return
     
-    @classmethod
-    def before_update(cls, parent, info, instance, data):
-        user: User = info.context.user
-        if not user.has_perm("evaluation.update_instructor"):
-            raise GraphQLError("You don't have permission")
-        return
+#     @classmethod
+#     def before_update(cls, parent, info, instance, data):
+#         user: User = info.context.user
+#         if not user.has_perm("evaluation.update_instructor"):
+#             raise GraphQLError("You don't have permission")
+#         return
     
-    @classmethod
-    def before_delete(cls, parent, info, instance, data):
-        user: User = info.context.user
-        if user.has_perm("evaluation.delete_instructor"):
-            raise GraphQLError("You don't have permission")
-        return
+#     @classmethod
+#     def before_delete(cls, parent, info, instance, data):
+#         user: User = info.context.user
+#         if user.has_perm("evaluation.delete_instructor"):
+#             raise GraphQLError("You don't have permission")
+#         return
 
 
-class EvaluationType(DjangoGrapheneCRUD):
-    """
-    A type for the `evaluation.Evaluation` model. 
-    """
+# class EvaluationType(DjangoGrapheneCRUD):
+#     """
+#     A type for the `evaluation.Evaluation` model. 
+#     """
 
-    class Meta:
-        model = Evaluation
-        exclude_fields = ('user', )
+#     class Meta:
+#         model = Evaluation
+#         exclude_fields = ('user', )
 
-    @classmethod
-    @login_required
-    def before_create(cls, parent, info, instance, data) -> None:
-        instructor_pk = data['instructor']['connect']['id']['exact']
+#     @classmethod
+#     @login_required
+#     def before_create(cls, parent, info, instance, data) -> None:
+#         instructor_pk = data['instructor']['connect']['id']['exact']
 
-        # the logged use is the evaluator 
-        instance.user = info.context.user
+#         # the logged use is the evaluator 
+#         instance.user = info.context.user
         
-        if Evaluation.objects.filter(user=info.context.user, instructor__pk=instructor_pk):
-            raise GraphQLError("You have evaluated this instructor before, you can edit it in My Evaluations.")
-        return
+#         if Evaluation.objects.filter(user=info.context.user, instructor__pk=instructor_pk):
+#             raise GraphQLError("You have evaluated this instructor before, you can edit it in My Evaluations.")
+#         return
 
-    # Forbid user to change other users' evaluation
-    @classmethod
-    @is_owner
-    def before_update(cls, parent, info, instance, data) -> None:
-        return
+#     # Forbid user to change other users' evaluation
+#     @classmethod
+#     @is_owner
+#     def before_update(cls, parent, info, instance, data) -> None:
+#         return
             
-    @classmethod
-    @is_owner
-    def before_delete(cls, parent, info, instance, data) -> None:
-        return
+#     @classmethod
+#     @is_owner
+#     def before_delete(cls, parent, info, instance, data) -> None:
+#         return
 
 
-class Data(ObjectType):
-    '''
-    This class is to provide general pourpose data.
-    '''
+# class Data(ObjectType):
+#     '''
+#     This class is to provide general pourpose data.
+#     '''
 
-    department_list = graphene.List(String, short=Boolean())
-    has_evaluated = graphene.Boolean(id=graphene.Int())
-    evaluated_instructors = graphene.List(graphene.String)
+#     department_list = graphene.List(String, short=Boolean())
+#     has_evaluated = graphene.Boolean(id=graphene.Int())
+#     evaluated_instructors = graphene.List(graphene.String)
 
-    @staticmethod
-    @login_required
-    def resolve_has_evaluated(parent, info, id):
-        return Evaluation.objects.filter(user=info.context.user, instructor__pk=id).exists()
+#     @staticmethod
+#     @login_required
+#     def resolve_has_evaluated(parent, info, id):
+#         return Evaluation.objects.filter(user=info.context.user, instructor__pk=id).exists()
 
-    @staticmethod
-    def resolve_department_list(parent, info, short=True):
-        dep_short = []
-        dep_long = []
-        for short_, long_ in departments:
-            dep_short.append(short_)
-            dep_long.append(long_)
+#     @staticmethod
+#     def resolve_department_list(parent, info, short=True):
+#         dep_short = []
+#         dep_long = []
+#         for short_, long_ in departments:
+#             dep_short.append(short_)
+#             dep_long.append(long_)
             
-        if short:
-            return dep_short
-        return dep_long
+#         if short:
+#             return dep_short
+#         return dep_long
     
-    @staticmethod
-    def resolve_evaluated_instructors(parent, info, short=True):
-        ids = []
-        for i in Instructor.objects.all():
-            if i.evaluation_set.exists():
-                ids.append(str(i.pk))
-        return ids
+#     @staticmethod
+#     def resolve_evaluated_instructors(parent, info, short=True):
+#         ids = []
+#         for i in Instructor.objects.all():
+#             if i.evaluation_set.exists():
+#                 ids.append(str(i.pk))
+#         return ids
 
 
 # Main entry for all the query types
 # Now only provides all Instructor & Evaluation objects
-class Query(Data, ObjectType):
+class Query:
+    ...
+    # evaluation = EvaluationType.ReadField()
+    # evaluations = EvaluationType.BatchReadField()
 
-    evaluation = EvaluationType.ReadField()
-    evaluations = EvaluationType.BatchReadField()
-
-    instructor = InstructorType.ReadField()
-    instructors = InstructorType.BatchReadField()
+    # instructor = InstructorType.ReadField()
+    # instructors = InstructorType.BatchReadField()
 
 
 # Main entry for all Mutation types
-class Mutation(ObjectType):
+class Mutation:
+    ...
+    # evaluation_create = EvaluationType.CreateField()
+    # evaluation_update = EvaluationType.UpdateField()
+    # evaluation_delete = EvaluationType.DeleteField()
 
-    evaluation_create = EvaluationType.CreateField()
-    evaluation_update = EvaluationType.UpdateField()
-    evaluation_delete = EvaluationType.DeleteField()
-
-    instructor_create = InstructorType.CreateField()
-    instructor_update = InstructorType.UpdateField()
-    instructor_delete = InstructorType.DeleteField()
+    # instructor_create = InstructorType.CreateField()
+    # instructor_update = InstructorType.UpdateField()
+    # instructor_delete = InstructorType.DeleteField()
