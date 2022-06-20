0325f0edc7f9c67771b986da9b1dfe3ace4c98f4
ammar-faifi <a@ammarf.com>
2022-06-20 17:06:39 +0300

    department list & has_evaluated queries

--- evaluation/schema.py
+++ evaluation/schema.py
@@ -15,6 +15,8 @@
 from typing import List
 
 import strawberry
+from strawberry import ID
+from strawberry.types.info import Info
 from strawberry_django_plus import gql
 
 from .types import InstructorType, EvaluationType
@@ -24,11 +26,9 @@ from .models import Evaluation, Instructor
 from data import departments
 
 
-
-
 # class InstructorType(DjangoGrapheneCRUD):
 #     '''
-#     A type for the `evaluation.Instructor` model. 
+#     A type for the `evaluation.Instructor` model.
 #     '''
 
 #     class Meta:
@@ -43,19 +43,19 @@ from data import departments
 #     @staticmethod
 #     def resolve_grading_avg(parent, info):
 #         return parent.avg()['grading__avg']
-    
+
 #     @staticmethod
 #     def resolve_teaching_avg(parent, info):
 #         return parent.avg()['teaching__avg']
-    
+
 #     @staticmethod
 #     def resolve_personality_avg(parent, info):
 #         return parent.avg()['personality__avg']
-    
+
 #     @staticmethod
 #     def resolve_overall(parent, info):
 #         return parent.avg()['overall']
-    
+
 #     @staticmethod
 #     def resolve_overall_float(parent, info):
 #         return parent.avg()['overall_float']
@@ -66,14 +66,14 @@ from data import departments
 #         if user.has_perm("evaluation.add_instructor"):
 #             raise GraphQLError("You don't have permission")
 #         return
-    
+
 #     @classmethod
 #     def before_update(cls, parent, info, instance, data):
 #         user: User = info.context.user
 #         if not user.has_perm("evaluation.update_instructor"):
 #             raise GraphQLError("You don't have permission")
 #         return
-    
+
 #     @classmethod
 #     def before_delete(cls, parent, info, instance, data):
 #         user: User = info.context.user
@@ -84,7 +84,7 @@ from data import departments
 
 # class EvaluationType(DjangoGrapheneCRUD):
 #     """
-#     A type for the `evaluation.Evaluation` model. 
+#     A type for the `evaluation.Evaluation` model.
 #     """
 
 #     class Meta:
@@ -96,9 +96,9 @@ from data import departments
 #     def before_create(cls, parent, info, instance, data) -> None:
 #         instructor_pk = data['instructor']['connect']['id']['exact']
 
-#         # the logged use is the evaluator 
+#         # the logged use is the evaluator
 #         instance.user = info.context.user
-        
+
 #         if Evaluation.objects.filter(user=info.context.user, instructor__pk=instructor_pk):
 #             raise GraphQLError("You have evaluated this instructor before, you can edit it in My Evaluations.")
 #         return
@@ -108,7 +108,7 @@ from data import departments
 #     @is_owner
 #     def before_update(cls, parent, info, instance, data) -> None:
 #         return
-            
+
 #     @classmethod
 #     @is_owner
 #     def before_delete(cls, parent, info, instance, data) -> None:
@@ -136,11 +136,11 @@ from data import departments
 #         for short_, long_ in departments:
 #             dep_short.append(short_)
 #             dep_long.append(long_)
-            
+
 #         if short:
 #             return dep_short
 #         return dep_long
-    
+
 #     @staticmethod
 #     def resolve_evaluated_instructors(parent, info, short=True):
 #         ids = []
@@ -148,6 +148,23 @@ from data import departments
 #             if i.evaluation_set.exists():
 #                 ids.append(str(i.pk))
 #         return ids
+
+
+def resolve_department_list(root, info: Info, short: bool = True) -> List[str]:
+    dep_short = []
+    dep_long = []
+    for short_, long_ in departments:
+        dep_short.append(short_)
+        dep_long.append(long_)
+
+    if short:
+        return dep_short
+
+    return dep_long
+
+
+def resolve_has_evaluated(root, info: Info, pk: ID) -> bool:
+    return Evaluation.objects.filter(user=info.context.request.user, instructor__pk=pk).exists()
 
 
 # Main entry for all the query types
@@ -160,11 +177,18 @@ class Query:
     instructor: InstructorType = gql.django.field()
     instructors: List[InstructorType] = gql.django.field()
 
+    department_list = strawberry.field(resolve_department_list)
+    has_evaluated = strawberry.field(resolve_has_evaluated)
+
+
+#     evaluated_instructors = graphene.List(graphene.String)
+
 
 # Main entry for all Mutation types
 @strawberry.type
 class Mutation:
     ...
+    # gql.django.mutation()
     # evaluation_create = EvaluationType.CreateField()
     # evaluation_update = EvaluationType.UpdateField()
     # evaluation_delete = EvaluationType.DeleteField()
