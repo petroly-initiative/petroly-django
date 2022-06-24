7ae9756a1fcba8609861a9372818f3b6ad4428a2
ammar-faifi <a@ammarf.com>
2022-06-24 20:18:18 +0300

    fix check_condition

--- account/types.py
+++ account/types.py
@@ -84,8 +84,8 @@ class OwnsObjPerm(ConditionDirective):
 
     message: Private[str] = "You don't own this object."
 
-    def check_condition(self, root: Any, info: GraphQLResolveInfo, user: UserType):
-        pk = info.variable_values["id"]
+    def check_condition(self, root: Any, info: GraphQLResolveInfo, user: UserType, **kwargs):
+        pk = kwargs["input"]["id"]
         if pk:
             try:
                 pk = int(pk)
