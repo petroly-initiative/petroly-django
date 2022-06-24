2c2345400d97e4c1010ca4e57e3e12b39433d331
ammar-faifi <a@ammarf.com>
2022-06-24 10:27:56 +0300

    clean imports

--- account/models.py
+++ account/models.py
@@ -1,11 +1,7 @@
-from tabnanny import verbose
-from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from PIL import Image
 from cloudinary.models import CloudinaryField
-from cloudinary import CloudinaryImage
 from data import departments, years
 from django.dispatch import receiver
 from django.db.models.signals import post_save
