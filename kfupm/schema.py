497e54dfa740968a25027bd73d238698942f6b4b
ammar-faifi <a@ammarf.com>
2022-06-25 13:25:22 +0300

    clean import

--- kfupm/schema.py
+++ kfupm/schema.py
@@ -1,29 +1,4 @@
-# import graphene
 import strawberry
-
-# import evaluation.schema
-# import account.schema
-# import forum.schema
-# import communities.schema
-# import roommate.schema
-
-
-# class Query(evaluation.schema.Query, account.schema.Query, forum.schema.Query, communities.schema.Query,
-#             roommate.schema.Query, graphene.ObjectType):
-#     # This class will inherit from multiple Queries
-#     # as we begin to add more apps to our project
-#     pass
-
-# class Mutation(evaluation.schema.Mutation, account.schema.Mutation, forum.schema.Mutation,
-#                 communities.schema.Mutation, roommate.schema.Mutation, graphene.ObjectType):
-
-#     pass
-
-
-# schema = graphene.Schema(query=Query, mutation=Mutation)
-
-
-# yourapp/schema.py
 
 import strawberry
 from strawberry.tools import merge_types
