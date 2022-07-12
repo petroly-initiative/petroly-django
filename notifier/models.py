e955bc5e32a906c23cb7d126a3aaf956c8856427
ammar-faifi <a@ammarf.com>
2022-07-12 09:29:20 +0300

    init creates models

--- notifier/models.py
+++ notifier/models.py
@@ -1,3 +1,72 @@
-from django.db import models
+"""
+Here are the related mdoels definition for the `notifier` app.
 
-# Create your models here.
+## Models:
+    - `Course` model, to abstract and store related info of each corse
+    from KFUPM API identify by its `crn`.
+        ### Fields:
+            - `crn`: a pk
+    - `TrackingList` model, to assign each users to what `Course` they are willing to track
+        ### Fields:
+            - `courses`
+            - `user`
+
+    - `NotificationEvent` model, to track the number, channel , and date of the notifications.
+        ### Fields:
+            - `user`
+            - `channel`
+            - `sent_on`
+"""
+
+from enum import Enum
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
+
+from django_choices_field import TextChoicesField
+
+
+class Course(models.Model):
+    """
+    It abstracts and store related info of each corse from KFUPM API identify by its `crn`.
+    pk: `crn`
+    """
+
+    crn = models.CharField(_("CRN"), max_length=5)
+
+
+class TrackingList(models.Model):
+    """
+    It assigns each users to what `Course` they are willing to track.
+    pk: django default `id`.
+    """
+
+    user = models.OneToOneField(
+        User, verbose_name=_("user"), on_delete=models.CASCADE
+    )
+    courses = models.ManyToManyField(Course, verbose_name=_("courses"))
+
+
+class NotificationEvent(models.Model):
+    """
+    It tracks the number, channel , and date of the notifications.
+    """
+
+    class ChannelEnum(Enum):
+        """Choices of `channel` as Enum"""
+
+        SMS = "sms", _("sms")
+        PUSH = "push", _("push")
+        EMAIL = "email", _("email")
+        WHATSAPP = "whatsapp", _("whatsapp")
+        TELEGRAM = "telegram", _("telegram")
+
+    user = models.ForeignKey(
+        User, verbose_name=_("user"), on_delete=models.CASCADE
+    )
+    channel = TextChoicesField(
+        verbose_name=_("channel"), max_length=15, choices_enum=ChannelEnum
+    )
+    sent_on = models.DateTimeField(
+        _("sent on"), auto_now=False, auto_now_add=True
+    )
