396d0b9f62c4771a36d9a7595989e9e1636c0f43
ammar-faifi <a@ammarf.com>
2022-07-12 12:21:33 +0300

    improve tracked_courses

--- notifier/schema.py
+++ notifier/schema.py
@@ -2,10 +2,13 @@
 This module is to define the GraphQL queries and mutations
 of the `notifier` app.
 """
+from typing import List
 
 from strawberry.scalars import JSON
+from strawberry.types import Info
 from strawberry_django_plus import gql
 
+from .types import CourseType
 from .utils import fetch_data
 
 @gql.type
@@ -15,6 +18,11 @@ class Query:
     @gql.field
     def raw_data(self, term: int, department: str) -> JSON:
         return fetch_data(term, department)
+
+    @gql.field
+    def tracked_courses(self, info: Info) -> List[CourseType]:
+        user = info.context.request.user
+        return user.tracking_list.courses
 
 
 @gql.type
