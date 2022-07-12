edd002aaa0f4de30f5ba2bd8140859de2040390b
ammar-faifi <a@ammarf.com>
2022-07-12 10:23:15 +0300

    add simple query

--- notifier/schema.py
+++ notifier/schema.py
@@ -3,10 +3,21 @@ This module is to define the GraphQL queries and mutations
 of the `notifier` app.
 """
 
+import json
+from strawberry.scalars import JSON
 from strawberry_django_plus import gql
 
-from .types import TrackingListType
+from .utils import fetch_data
 
 @gql.type
 class Query:
     """Main entry of all Query types of `notifier` app."""
+
+    @gql.field
+    def raw_data(self, term: int, department: str) -> JSON:
+        return fetch_data(term, department)
+
+
+@gql.type
+class Mutation:
+    """Main entry of all Mutation types of `notifier` app."""
