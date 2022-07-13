b3bd10badd62ef93064a55d9c7c08bfc1a2f2917
ammar-faifi <a@ammarf.com>
2022-07-13 11:57:15 +0300

    add filter for admin site

--- notifier/admin.py
+++ notifier/admin.py
@@ -3,16 +3,21 @@ Customization of the admin site for `notifier` app.
 """
 
 from django.contrib import admin
-from .models import TrackingList, Course, NotificationEvent
+from . import models
 
-admin.site.register([Course, NotificationEvent])
+admin.site.register(
+    [
+        models.Course,
+        models.NotificationEvent,
+        models.NotificationChannel,
+    ]
+)
 
-@admin.register(TrackingList)
+
+@admin.register(models.TrackingList)
 class TrackingListAdmin(admin.ModelAdmin):
     """
     Custom settings for `TrackingList` app in admin site.
     """
 
-    filter_horizontal = [
-        "courses",
-    ]
+    filter_horizontal = ["courses", "channels"]
