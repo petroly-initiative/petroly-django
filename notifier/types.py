2ceb315c7e8f3a703f476b85874cb1a24d407e3a
ammar-faifi <a@ammarf.com>
2022-07-14 10:55:03 +0300

    add update_tracking_list

--- notifier/types.py
+++ notifier/types.py
@@ -3,20 +3,86 @@ This module defines the types pf this `notifier` app
 to be used from `schema` module.
 """
 
-from strawberry import auto
-from strawberry_django_plus import gql
+import dataclasses
+from typing import List, Any
 
-from .models import Course, TrackingList
+from strawberry import auto, ID, Private
+from strawberry_django_plus import gql
+from strawberry_django_plus.permissions import ConditionDirective
+from strawberry_django_plus.utils.typing import UserType
+from graphql.type.definition import GraphQLResolveInfo
+
+
+from .models import Course, TrackingList, NotificationChannel
+
+
+@gql.django.type(NotificationChannel)
+class NotificationChannelType:
+    """A type for `NotificationChannel` model."""
+
+    channel: auto
+
+
+@gql.django.input(NotificationChannel)
+class NotificationChannelInput:
+    """Am input type for `NotificationChannel` model."""
+
+    channel: auto
+
 
 @gql.django.type(Course)
 class CourseType:
     """A type for `Course` model."""
 
     crn: auto
+    # term: auto
+    # department: auto
+
+
+@gql.django.input(Course)
+class CourseInput:
+    """A type for `Course` model."""
+
+    crn: auto
+    term: auto
+    department: auto
+
 
 @gql.django.type(TrackingList)
 class TrackingListType:
     """A type for `TrackingList` model."""
 
-    user: auto
-    courses: auto
+    courses: List[CourseType]
+    channels: List[NotificationChannelType]
+
+
+@gql.django.input(TrackingList, partial=True)
+class TrackingListInput:
+    """An input type for `TrackingList` model."""
+
+    pk: ID
+    courses: List[CourseInput]
+    # channels: List[NotificationChannelInput]
+
+
+@dataclasses.dataclass
+class MatchIdentity(ConditionDirective):
+    """
+    This checks wether the provided `pk` field
+    which is also user's pk match the logged in user.
+    """
+
+    message: Private[str] = "Your identity aren't matching the provided `pk` field."
+
+    def check_condition(
+        self, root: Any, info: GraphQLResolveInfo, user: UserType, **kwargs
+    ):
+        pk = kwargs["input"]["pk"]
+        if pk:
+            try:
+                if int(pk) == user.pk:
+                    return True
+                return False
+            except Exception as exc:
+                raise ValueError("The field `pk` is not valid.") from  exc
+        raise ValueError("The field `pk` must be provided.")
