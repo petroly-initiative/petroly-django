3641d40165c3aa6d5730c71434b1c2338bc5a28a
ammar-faifi <a@ammarf.com>
2022-07-14 08:25:07 +0300

    `tracked_courses` return correct data

--- notifier/schema.py
+++ notifier/schema.py
@@ -2,14 +2,14 @@
 This module is to define the GraphQL queries and mutations
 of the `notifier` app.
 """
-from typing import List
 
 from strawberry.scalars import JSON
 from strawberry.types import Info
 from strawberry_django_plus import gql
+from strawberry_django_plus.permissions import IsAuthenticated
 
-from .types import CourseType
 from .utils import fetch_data
+
 
 @gql.type
 class Query:
@@ -17,12 +17,57 @@ class Query:
 
     @gql.field
     def raw_data(self, term: int, department: str) -> JSON:
+        """
+        The raw data directly from the API.
+        """
+
         return fetch_data(term, department)
 
-    @gql.field
-    def tracked_courses(self, info: Info) -> List[CourseType]:
+    @gql.field(directives=[IsAuthenticated()])
+    def tracked_courses(self, info: Info) -> JSON:
+        """get all tracked courses' CRNs by the
+        current logged in user.
+
+        Returns:
+            List[CourseType]: List of courses CRNs
+        """
+
         user = info.context.request.user
-        return user.tracking_list.courses
+        courses = user.tracking_list.courses.all()
+
+        result = []
+        for course in courses:
+            for raw_course in fetch_data(course.term, course.department):
+                if course.crn == raw_course["crn"]:
+                    result.append(raw_course)
+
+        return result
+
+    @gql.field
+    def search(self, term: int, department: str, title: str) -> JSON:
+        """to call `fetch_data`
+        and perform case-insensitive searching
+        in the `course_number` and `course_title` fields from API data.
+
+        Args:
+            term (int): term number
+            department (str): in which department
+            title (str): case-insensitive title of the course or code
+
+        Returns:
+            JSON: the same structure of the API data.
+        """
+
+        raw = fetch_data(term, department)
+        result = []
+        for course in raw:
+            if (
+                title.lower() in course["course_number"].lower()
+                or title.lower() in course["course_title"].lower()
+            ):
+                result.append(course)
+
+        return result
 
 
 @gql.type
