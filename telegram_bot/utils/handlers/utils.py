3ed2759d763f3774e2fe35cf6339d030b206f475
ammar-faifi <a@ammarf.com>
2022-07-21 14:05:46 +0300

    typos and formattings

--- telegram_bot/utils/handlers/utils.py
+++ telegram_bot/utils/handlers/utils.py
@@ -1,21 +1,26 @@
+"""
+This module contains some utilities for handling telegram commands
+"""
+
 from typing import List
-from telegram import InlineKeyboardMarkup
 
 
-def populateTracking(courses: List) -> str:
-    """helper method to create a formated message for each course in the tracking list"""
+def populate_tracking(courses: List) -> str:
+    """helper method to create a formatted message for each course in the tracking list"""
     # for each course we will create a message format
     result = ""
     for course in courses:
-        result += f"""**{course["course_number"]}\-{course["section_number"]}**  \- *{course["crn"]}*
+        result += f"""**{course["course_number"]}\\-{course["section_number"]}**  \\- *{course["crn"]}*
         Available Seats: {course["available_seats"]}
-        Waitlist: {conditionalColoring(course["waiting_list_count"])}\n\n"""
+        Waiting list: {conditional_coloring(course["waiting_list_count"])}\n\n"""
     return result
 
 
-def conditionalColoring(waitlist_count) -> str:
-    """helper method to append the correct coloring according to the waitlist count"""
-    if waitlist_count == 5:
+def conditional_coloring(wait_list_count) -> str:
+    """helper method to append the correct coloring according to
+    the waiting list count"""
+
+    if wait_list_count == 5:
         return "🔴 Closed"
-    else:
-        return "🟢 Open"
+
+    return "🟢 Open"
