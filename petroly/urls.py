80e1d186b7a6566fa55fcc4fdbd5f35abe879c14
ammar-faifi <a@ammarf.com>
2022-07-21 13:13:49 +0300

    clean white lines

--- petroly/urls.py
+++ petroly/urls.py
@@ -13,16 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path,include 
-from django.contrib.staticfiles.urls import static
-from petroly.settings import dev
-import debug_toolbar
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views.generic.base import TemplateView 
-
 from strawberry.django.views import GraphQLView
+
 from .schema import schema
 
 
@@ -38,8 +35,3 @@ urlpatterns = [
     path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path("bot/", include("telegram_bot.urls"))
 ]
-
-
-
-
-
