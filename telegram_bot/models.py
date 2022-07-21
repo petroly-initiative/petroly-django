2fb8361b153b7077ba38ffc6aaca2cbdadbc4bb5
ammar-faifi <a@ammarf.com>
2022-07-21 15:52:06 +0300

    rearrange the models

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,11 +1,23 @@
+"""
+All `telegram_bot` models.
+
+models: 
+    - TelegramProfile
+"""
+
 from django.db import models
 
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-from account.models import Profile
-# Create your models here.
-class TelegramBotUser(models.Model):
-    "A class to represent the link between a django user and the telegram account"
-    telegram_user_id = models.CharField(default="", max_length=256);
-    telegram_username=  models.CharField(primary_key=True, max_length=256);
-    user = models.OneToOneField(Profile, on_delete= models.PROTECT)
+class TelegramProfile(models.Model):
+    """
+    A class to represent the link between a django user and the telegram account
+    """
+
+    telegram_user_id = models.CharField(_("telegram user ID"), max_length=256)
+    telegram_username = models.CharField(_("telegram username"), max_length=256)
+
+    user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
