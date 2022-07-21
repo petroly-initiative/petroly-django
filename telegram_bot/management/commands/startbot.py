7c1c946cf5f9bcf6b8b95e830817c744007864f0
ammar-faifi <a@ammarf.com>
2022-07-21 13:53:20 +0300

    clean codes

--- telegram_bot/management/commands/startbot.py
+++ telegram_bot/management/commands/startbot.py
@@ -1,12 +1,13 @@
-from django.core.management.base import BaseCommand;
-from telegram_bot.utils.bot_controller import BotController;
+from django.core.management.base import BaseCommand
+from telegram_bot.utils.bot_controller import BotController
+
 
 class Command(BaseCommand):
     """a command that fires the bot polling"""
+
     help = "Runs the telegram bot process"
 
     def handle(self, *args, **options):
         self.stdout.write("Telegram Bot running...")
-        controller = BotController();
-        self.stdout.write("Bot Process terminated");
-        
+        BotController()
+        self.stdout.write("Bot Process terminated")
