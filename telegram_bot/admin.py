767dd87b9231222811acf0e7bb0e5bb6cf6a6f77
ammar-faifi <a@ammarf.com>
2022-07-22 09:40:56 +0300

    add Token model

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import TelegramProfile, Token
 
-# Register your models here.
+admin.site.register(
+    [
+        TelegramProfile,
+    ]
+)
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+
+    list_display = ["token", "user"]
---
+++ telegram_bot/schema.py
@@ -0,0 +1,32 @@
+"""
+This module is to define the GraphQL queries and mutations
+of the `telegram_bot` app.
+"""
+
+from strawberry.types import Info
+from strawberry_django_plus import gql
+from strawberry_django_plus.permissions import IsAuthenticated
+
+from .models import Token
+
+
+@gql.type
+class Query:
+    """Main entry of all Query types of `notifier` app."""
+
+
+@gql.type
+class Mutation:
+    """Main entry of all Mutation types of `notifier` app."""
+
+    @gql.mutation(directives=[IsAuthenticated()])
+    def get_telegram_token(self, info:Info) -> str:
+        """
+        Create a obj from `Token` model,
+        return the generated token.
+        """
+        user = info.context.request.user
+
+        obj = Token.objects.create(user=user)
+
+        return obj.token
