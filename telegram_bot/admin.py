a2a981d445cfdce46096597ea3060883a19e86f6
ammar-faifi <a@ammarf.com>
2022-08-04 18:12:47 +0300

    use custom Admin model

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from .models import TelegramProfile, Token
 
-admin.site.register(
-    [
-        TelegramProfile,
-    ]
-)
-
 
 @admin.register(Token)
 class TokenAdmin(admin.ModelAdmin):
 
     list_display = ["token", "user"]
+
+
+@admin.register(TelegramProfile)
+class TelegramProfileAdmin(admin.ModelAdmin):
+    """
+    Custom settings for `TelegramProfile` app in admin site.
+    """
+
+    list_display = [
+        "user",
+        "username",
+    ]
