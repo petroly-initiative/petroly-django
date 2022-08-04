3fda6bee39344613c73c0f8f5fe44815fe53cd16
ammar-faifi <a@ammarf.com>
2022-08-04 18:09:55 +0300

    use custom Admin models

--- notifier/admin.py
+++ notifier/admin.py
@@ -7,9 +7,7 @@ from . import models
 
 admin.site.register(
     [
-        models.Course,
         models.NotificationEvent,
-        models.Term
     ]
 )
 
@@ -20,4 +18,45 @@ class TrackingListAdmin(admin.ModelAdmin):
     Custom settings for `TrackingList` app in admin site.
     """
 
-    filter_horizontal = ["courses", ]
+    list_display = [
+        "id",
+        "user",
+        "channels",
+    ]
+    list_filter = [
+        "channels",
+    ]
+    filter_horizontal = [
+        "courses",
+    ]
+
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    """
+    Custom settings for `Course` app in admin site.
+    """
+
+    list_display = [
+        "crn",
+        "term",
+        "available_seats",
+        "waiting_list_count",
+        "department",
+    ]
+    list_filter = [
+        "term",
+        "department",
+    ]
+
+
+@admin.register(models.Term)
+class TermAdmin(admin.ModelAdmin):
+    """
+    Custom settings for `Term` app in admin site.
+    """
+
+    list_display = [
+        "short",
+        "allowed",
+    ]
