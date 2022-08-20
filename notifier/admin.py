2bc4af2c7fe56096c8dd61f61052e94580a7a062
ammar-faifi <a@ammarf.com>
2022-08-20 09:39:51 +0300

    custom admin for `Cache`

--- notifier/admin.py
+++ notifier/admin.py
@@ -8,7 +8,6 @@ from . import models
 admin.site.register(
     [
         models.NotificationEvent,
-        models.Cache,
     ]
 )
 
@@ -29,6 +28,28 @@ class TrackingListAdmin(admin.ModelAdmin):
     ]
     filter_horizontal = [
         "courses",
+    ]
+
+@admin.decorators.display(description="Valid", boolean=True)
+def valid(obj) -> bool:
+    return obj.is_valid()
+
+@admin.register(models.Cache)
+class CacheAdmin(admin.ModelAdmin):
+    """
+    Custom settings for `Cache` model in admin site.
+    """
+
+    list_display = [
+        "id",
+        "term",
+        "department",
+        "updated_on",
+        valid,
+    ]
+    list_filter = [
+        "term",
+        "department",
     ]
 
 
