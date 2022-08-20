004907a95d60be81dd80d33bf706567a62b50807
ammar-faifi <a@ammarf.com>
2022-08-20 18:59:53 +0300

    increase timeout

--- notifier/utils.py
+++ notifier/utils.py
@@ -65,7 +65,7 @@ def request_data(term, department) -> None:
             API,
             params={"term_code": term, "department_code": department},
             proxies=proxies,
-            timeout=30,
+            timeout=35,
         )
 
     except rq.Timeout:
