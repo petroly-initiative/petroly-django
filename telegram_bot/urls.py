aabca5b6bcb40f618bb19d04689eee69e359d10a
ammar-faifi <a@ammarf.com>
2022-08-31 09:22:06 +0300

    simple mass telegram message

--- telegram_bot/urls.py
+++ telegram_bot/urls.py
@@ -1,5 +1,10 @@
-app_name="telegram_bot"
+
+
+from django.urls import path
+from . import views
+
+app_name = "telegram_bot"
 
 urlpatterns = [
-    
+    path('send_mass_telegram/', views.SendMassTelegramView.as_view(), name='send_mass_telegram'),
 ]
--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import FormView
+
+from .forms import TelegramMessageForm
+
+
+class SendMassTelegramView(FormView):
+    """a view to send a mass message to all
+    saved telegram accounts"""
+
+    template_name = "telegram_bot/telegram_form.html"
+    form_class = TelegramMessageForm
+
+    def form_valid(self, form: TelegramMessageForm):
+        form.send_message(self.request.POST["message"])
+
+        return render(self.request, "telegram_bot/telegram_sent.html")
