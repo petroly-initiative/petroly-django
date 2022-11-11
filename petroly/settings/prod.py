bd0b995ec08825a89f169fa6ca18286de475415f
ammar-faifi <a@ammarf.com>
2022-11-11 16:52:33 +0300

    remove deprecated `django-heroku`

--- petroly/settings/prod.py
+++ petroly/settings/prod.py
@@ -1,15 +1,25 @@
 import os
 import re
-from pathlib import Path
 
 import dj_database_url
-import django_heroku
 import cloudinary
+from django.test.runner import DiscoverRunner
 
 from .base import *
 
-# Extra app
+IS_HEROKU = "DYNO" in os.environ
 
+
+# Generally avoid wildcards(*). However since Heroku router provides hostname validation it is ok
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG", 'False') == 'True'
+
+if IS_HEROKU:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = []
+
+# Extra app
 INSTALLED_APPS += [
     'django.contrib.admindocs',
 ]
@@ -22,15 +32,13 @@ MIDDLEWARE += [
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL", default='')
 
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-DEBUG = os.environ.get("DEBUG", 'False') == 'True'
 
 ALLOWED_HOSTS = ['.petroly.co', '.petroly-main.herokuapp.com']
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+# CORS lib
 CORS_ALLOWED_ORIGINS = ['https://petroly.vercel.app', 'https://react.petroly.co', 'https://petroly.co']
 
 
@@ -45,13 +53,49 @@ cloudinary.config(
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 # Check if DATABASE_URL is provided
 # otherwise fallback to basic db
+MAX_CONN_AGE = 600
+
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=500)
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    }
 }
 
+if "DATABASE_URL" in os.environ:
+    # Configure Django for DATABASE_URL environment variable.
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=MAX_CONN_AGE, ssl_require=True)
 
-STATIC_URL = '/static/'
+    # Enable test database if found in CI environment.
+    if "CI" in os.environ:
+        DATABASES["default"]["TEST"] = DATABASES["default"]
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
 MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
+# Enable WhiteNoise's GZip compression of static assets.
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# Test Runner Config
+class HerokuDiscoverRunner(DiscoverRunner):
+    """Test Runner for Heroku CI, which provides a database for you.
+    This requires you to set the TEST database (done for you by settings().)"""
+
+    def setup_databases(self, **kwargs):
+        self.keepdb = True
+        return super(HerokuDiscoverRunner, self).setup_databases(**kwargs)
+
+
+# Use HerokuDiscoverRunner on Heroku CI
+if "CI" in os.environ:
+    TEST_RUNNER = "gettingstarted.settings.HerokuDiscoverRunner"
+
 
 # To get Email when >500 error happens
 SERVER_EMAIL = 'Error@petroly.co'
@@ -67,6 +111,3 @@ IGNORABLE_404_URLS = [
     re.compile(r'^/robots\.txt$'),
     re.compile(r'^/ads\.txt$'),
 ]
-
-
-django_heroku.settings(locals(), logging=False)
