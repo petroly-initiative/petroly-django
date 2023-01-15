37eb337709d2305217a4223e107db9bb2cce4e00
ammar-faifi <a@ammarf.com>
2023-01-15 21:30:55 +0300

    fix GEOL

--- data.py
+++ data.py
@@ -90,5 +90,6 @@ class SubjectEnum(TextChoices):
     STAT = ("STAT", "Statistics")
     CGS = ("CGS", "College of General Studies")
     ECON = ("ECON", "Economics")
+    GEOL = ("GEOL", "Geology")
 
     # TODO make sure to map new subjects in instructor search
