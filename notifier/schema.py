e21a95457eed515bf9c870dbd2bc0517b87cadb9
ammar-faifi <me@ammarf.sa>
2023-11-07 14:25:22 +0300

    reduce repeats to 10

--- notifier/schema.py
+++ notifier/schema.py
@@ -154,7 +154,7 @@ class Mutation:
                 "notifier.utils.check_session",
                 user.pk,
                 repeats=-1,
-                minutes=15,
+                minutes=10,
                 next_run=now(),
                 schedule_type=Schedule.MINUTES,
                 name=f"save-cookies-{user.pk}",
