ed61f2e7bf601ee0208a830fbc4a271acce4e155
ammar-faifi <me@ammarf.sa>
2024-01-04 11:30:49 +0300

    first stage of API and test with OpenAI

---
+++ whatsguard/types.py
@@ -0,0 +1,38 @@
+"""
+This module defines the types pf this `whatsguard` app
+to be used from `schema` module.
+"""
+
+
+import strawberry
+import strawberry.django
+from strawberry import auto
+from strawberry.types import Info
+
+from .models import Chat, Contact, Message
+
+
+@strawberry.django.input(Message)
+class MessageType:
+    """A type for `Message` model."""
+
+    author: auto
+    from_id: auto
+    body: auto
+    device_type: auto
+
+
+@strawberry.django.input(Chat)
+class ChatType:
+    """A type for `Chat` model."""
+
+    name: auto
+    is_group: auto
+
+
+@strawberry.django.input(Contact)
+class ContactType:
+    """A type for `Contact` model."""
+
+    number: auto
+    pushname: auto
